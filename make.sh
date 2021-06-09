a1025d8ad8328b44d8a6a9be43bd6898b29e5dfc
Kovalenko Vitaliy <80783686+VAMarechal@users.noreply.github.com>
2021-06-09 22:52:47 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-outFile="Files.txt"
+outFile="index.html"
 echo '<html><head>' > $outFile 
 echo '    <title>Panorams</title>' >> $outFile
 echo '</head><body align="center">' >> $outFile
-
+ 
 for file in `find img/ -type f -name "*.jpg" | sort -gr |  cut -d'/' -f2`
 do
-    echo '<H3>'${file}'</H3>' ;
-    echo '<iframe width="600" height="400" allowfullscreen style="border-style:none;" src="pannellum/pannellum.htm#panorama=../img/'${file}'&autoLoad=true"></iframe>' ;
-    # echo '<p>' ;
+     echo '<H3>'${file}'</H3>' ;
+     echo '<iframe width="600" height="400" allowfullscreen style="border-style:none;" src="lib/pannellum.htm#panorama=../img/'${file}'&autoLoad=true"></iframe>' ;
+     # echo '<p>' ;
 done  >> $outFile
-
+ 
 echo '</body></html>' >> $outFile
